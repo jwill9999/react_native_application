aacca88e791500f50e28245c9f4f30ae40570d79
Jason Williams <jasonwilliams2308@gmail.com>
2018-03-30 20:10:53 +0100

    Text Input, Internal state

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, TextInput, Button, Text } from "react-native";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "", placeName: [] };
+  }
+  buttonPress = () => {
+    if (this.state.text.trim() === "") {
+      return;
+    }
+    this.setState(prevState => ({
+      placeName: prevState.placeName.concat(prevState.text),
+      text: ""
+    }));
+  };
+
   render() {
+    const places = this.state.placeName.map(place => (
+      <Text key={place}>{place}</Text>
+    ));
     return (
       <View style={styles.container}>
-        <Text>Welcome to my first React Native application </Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+            placeholder="Add Your Text"
+          />
+          <Button
+            style={styles.button}
+            title="Add"
+            onPress={this.buttonPress}
+          />
+        </View>
+        <View>{places}</View>
       </View>
     );
   }
@@ -14,8 +44,22 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 36,
     backgroundColor: "#f8f6f6",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "flex-start"
+  },
+  input: {
+    width: "70%",
+    height: 40
+  },
+  inputContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  button: {
+    width: "30%"
   }
 });
